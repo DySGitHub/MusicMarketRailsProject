d33a097728f2acdf99239eb3e8cbbeecd00e6475
Dylan Sage <20063614@mail.wit.ie>
2016-12-22 21:44:39 +0000

    Added Guitar Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
       root 'static_pages#home'
          
        get  '/home',    to: 'static_pages#home'
+       get  '/pianos',    to: 'static_pages#pianos'
+       get  '/guitars',    to: 'static_pages#guitars'
+       get  '/accessories',    to: 'static_pages#accessories'
       get  '/help',    to: 'static_pages#help'
       get  '/about',   to: 'static_pages#about'
       get  '/contact', to: 'static_pages#contact'
