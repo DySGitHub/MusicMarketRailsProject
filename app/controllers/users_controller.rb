df510249ad3d61ca08a806d2f4a5b73b9275e344
Dylan Sage <20063614@mail.wit.ie>
2017-01-03 01:55:46 +0000

    Added site security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
   class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :show]
+        before_action :correct_user,   only: [:edit, :update]
+
  
       def show
         @user = User.find(params[:id])
@@ -20,6 +22,11 @@
       def new
         @user = User.new
   
+    end
+      
+      def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
       
      def create
